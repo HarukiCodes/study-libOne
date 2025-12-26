e8e874c8e4ce52f0fee3ee23a9dfcc76abe2191b
SHaruGames <haruking.86kun@gmail.com>
2025-12-27 01:17:35 +0900

    02 window()&clear()

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,28 +1,21 @@
 #include "libOne.h"
 
+#define RED 255, 0, 0
+#define GREEN 0, 255, 0
+#define BLUE 0, 0, 255
+#define CYAN 0, 255, 255
+#define MAGENTA 255, 0, 255
+#define YELLOW 255, 255, 0
+
 void gmain()
 {
     const float wnd_w = 800;
     const float wnd_h = 600;
-    const float cell_size = 50;
 
     window(wnd_w, wnd_h);
 
-    for (int i = 0; i < wnd_w / cell_size; i++)
-    {
-        for (int j = 0; j < wnd_h / cell_size; j++)
-        {
-            if ((i + j) % 2 == 0)
-            {
-                fill(0);
-            }
-            else
-            {
-                fill(0, 100, 0);
-            }
-            rect(cell_size * i, cell_size * j, cell_size, cell_size);
-        }
-    }
+    // window‚ðì¬‚µ‚Ä‚©‚çclear()‚ðŒÄ‚Ô
+    clear(YELLOW);
 
     pause();
 }
