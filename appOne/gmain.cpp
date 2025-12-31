0bec5fb59d2c7a5c7ac8201509125b17b0202f5a
SHaruGames <haruking.86kun@gmail.com>
2025-12-31 16:36:27 +0900

    08 Have fun learning by drawing shapes(for,while)

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,37 +1,56 @@
 #include "color_def.h"
 #include "libOne.h"
 
+#define BGCOLOR 74, 84, 89
+
 void gmain()
 {
     const float wnd_w = 1280, wnd_h = 720;
     window(wnd_w, wnd_h, full);
-    float px = wnd_w / 2;
-    float py = wnd_h / 2;
-    float vx = 5;
-    float radius = 100;
-    float rect_len = radius * Sqrt(2);
-    float sw = radius / 8;  // strokeWeight
-    float angle = 0;
-    float angle_speed = 0.03f;
 
-    rectMode(CENTER);
-    stroke(RED);
-
+    // Data
+    int life = 5;
+    float px = 500;
+    float py = 140;
+    float radius = 50;
+    float space = 100;
+    float sw = 20;
     while (notQuit)
     {
-        px += vx;
-        angle += angle_speed;
-        if (px < 0 || px > wnd_w)
+        // Update data
+        if (isTrigger(KEY_A))
         {
-            vx = -vx;
-            angle_speed = -angle_speed;
+            life--;
         }
-        clear(GRAY);
+        if (isTrigger(KEY_D))
+        {
+            life++;
+        }
+        // Drawing
+        clear(BGCOLOR);
+        fill(YELLOW);
+        print((let) "life=" + life);
         strokeWeight(sw);
-        circle(px, py, radius * 2);
-        rect(px, py, rect_len, rect_len, angle);
-        line(wnd_w / 2, 0, px, py);
-        strokeWeight(sw * 3);
-        point(px, py);
+        stroke(WHITE);
+
+        // while-version
+        int i = 0;
+        fill(255, 200, 200);
+        while (i < life)
+        {
+            // offset: Šî€‚©‚ç‚Ì‚¸‚ê
+            float offset_x = space * i;
+            circle(px + offset_x, py, radius * 2);
+            i++;
+        }
+
+        // for-version
+        fill(160, 200, 240);
+        for (int i = 0; i < life; i++)
+        {
+            float offset_x = space * i;
+            float offset_y = space * 2;
+            circle(px + offset_x, py + offset_y, radius * 2);
+        }
     }
 }
