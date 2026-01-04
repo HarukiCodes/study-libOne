c3d86db62c0855c673ad7cb97c7dc24fa576a46e
SHaruGames <haruking.86kun@gmail.com>
2026-01-04 21:10:33 +0900

    今までのサンプルコードを関数化した。

---
+++ appOne/libone_samples.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace libone_samples
+{
+    void _01_Intro();
+    void _02_UseWindow_Clear();
+    void _03_UsePoint();
+    void _04_UseLine();
+    void _05_UseCircle();
+    void _06_UseRect();
+    void _07_DrawWheel();
+    void _08_DrawLifeCircle();
+    void _09_DrawHPGauge();
+    void _10to12_DrawSquareOrRoundFaces();
+}  // namespace libone_sample
